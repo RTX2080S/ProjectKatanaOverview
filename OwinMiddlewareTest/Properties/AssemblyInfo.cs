25b8f0f612ba6146c8d41669f66ffba711d835ea
LL <nzlili2015@gmail.com>
2017-05-09 10:44:55 +1200

    Add iis pipelines example

---
+++ OwinMiddlewareTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Owin Middleware Test")]
+[assembly: AssemblyDescription("Owin Middleware Test in IIS Pipelines")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alien Laboratory")]
+[assembly: AssemblyProduct("Owin Middleware Test")]
+[assembly: AssemblyCopyright("Copyright © Alien Laboratory 2017")]
+[assembly: AssemblyTrademark("ALI")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8d642b58-72d9-4f20-9d5e-80d6bb855560")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ OwinMiddlewareTest/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.IO;
+using System.Web;
+using Microsoft.Owin.Extensions;
+
+[assembly: OwinStartup(typeof(OwinMiddlewareTest.Startup))]
+
+namespace OwinMiddlewareTest
+{
+    public class Startup
+    {
+        /// The output window will display: 
+        /// Current IIS event: AuthenticateRequest Msg: Middleware 1
+        /// Current IIS event: AuthenticateRequest Msg: 2nd MW
+        /// Current IIS event: ResolveRequestCache Msg: 3rd MW
+        /// <see cref="https://docs.microsoft.com/en-us/aspnet/aspnet/overview/owin-and-katana/owin-middleware-in-the-iis-integrated-pipeline"/>
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use((context, next) =>
+            {
+                PrintCurrentIntegratedPipelineStage(context, "Middleware 1");
+                return next.Invoke();
+            });
+            app.Use((context, next) =>
+            {
+                PrintCurrentIntegratedPipelineStage(context, "2nd MW");
+                return next.Invoke();
+            });
+
+            /// Configures all the previously registered middleware components (in this case, our two diagnostic components) 
+            /// to run on the authentication stage of the pipeline
+            app.UseStageMarker(PipelineStage.Authenticate);
+
+            app.Run(context =>
+            {
+                PrintCurrentIntegratedPipelineStage(context, "3rd MW");
+                return context.Response.WriteAsync("Hello world \nPlease check your Visual Studio output window for Debug output");
+            });
+
+            /// The EARLIEST stage of calls to app.UseStageMarker wins. 
+            /// For example, if you switch the order of app.UseStageMarker calls from this example,
+            /// The output window will display: 
+            /// Current IIS event: AuthenticateRequest Msg: Middleware 1
+            /// Current IIS event: AuthenticateRequest Msg: 2nd MW
+            /// Current IIS event: AuthenticateRequest Msg: 3rd MW
+            app.UseStageMarker(PipelineStage.ResolveCache);
+        }
+
+        private void PrintCurrentIntegratedPipelineStage(IOwinContext context, string msg)
+        {
+            var currentIntegratedpipelineStage = HttpContext.Current.CurrentNotification;
+            context.Get<TextWriter>("host.TraceOutput").WriteLine($"Current IIS event: { currentIntegratedpipelineStage } Msg: { msg }");
+        }
+    }
+}
