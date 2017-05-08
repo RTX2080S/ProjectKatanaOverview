7b795a5876bb4fa53293263201fac7f2c0a85844
LL <nzlili2015@gmail.com>
2017-05-08 15:10:54 +1200

    moving from a Web-server host to a command line host

---
+++ SimpleKatanaServer/StartWebServer.ps1
@@ -0,0 +1,5 @@
+﻿echo "Starting the Web-server host from Powershell"
+echo "Read more at: "
+echo "https://docs.microsoft.com/en-us/aspnet/aspnet/overview/owin-and-katana/an-overview-of-project-katana#switching-hosts"
+echo ""
+ ..\packages\OwinHost.3.1.0\tools\OwinHost.exe
