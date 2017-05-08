3d0f2d9d328c2cb8edfc03cdea8692a6d7cf803b
LL <nzlili2015@gmail.com>
2017-05-08 16:22:50 +1200

    Add the error page middleware to the pipeline.

--- SelfHostOwinApp/Startup1.cs
+++ SelfHostOwinApp/Startup1.cs
@@ -10,9 +10,18 @@ namespace SelfHostOwinApp
     public class Startup1
     {
         public void Configuration(IAppBuilder app)
-        {
+        {    
+            // New code: Add the error page middleware to the pipeline. 
+            app.UseErrorPage();
+
             app.Run(context =>
-            {
+            {  
+                // New code: Throw an exception for this URI path.
+                if (context.Request.Path.Value == "/fail")
+                {
+                    throw new Exception("Random exception");
+                }
+
                 context.Response.ContentType = "text/plain";
                 return context.Response.WriteAsync("Hello, world.");
             });
