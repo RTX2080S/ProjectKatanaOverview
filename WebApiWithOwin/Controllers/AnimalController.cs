25b5024cf24a8bb5732611c0e5610d2bf9ff64f4
LL <nzlili2015@gmail.com>
2017-05-09 14:25:45 +1200

    create web api with owin project

---
+++ WebApiWithOwin/Controllers/AnimalController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiWithOwin.Models;
+
+namespace WebApiWithOwin.Controllers
+{
+    public class AnimalController : ApiController
+    {
+        public IEnumerable<Animal> GetAnimals()
+        {
+            var animals = new List<Animal>() {
+                new Animal() { Name = "Dog", Description = "A dog. " },
+                new Animal() { Name = "Cat", Description = "A cat. " },
+                new Animal() { Name = "Bird", Description = "A bird. " }
+            };
+
+            return animals;
+        }
+    }
+}
---
+++ WebApiWithOwin/Models/Animal.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiWithOwin.Models
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }        
+    }
+}
