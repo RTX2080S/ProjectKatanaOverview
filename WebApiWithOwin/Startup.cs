60178b4ffb92f36cce9964208d70ec6423f1ec8e
LL <nzlili2015@gmail.com>
2017-05-09 14:30:53 +1200

    Insert Owin middlewares

---
+++ WebApiWithOwin/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.IO;
+using System.Web;
+using Microsoft.Owin.Extensions;
+
+[assembly: OwinStartup(typeof(WebApiWithOwin.Startup))]
+
+namespace WebApiWithOwin
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use((context, next) =>
+            {
+                PrintCurrentIntegratedPipelineStage(context, "Middleware 1");
+                return next.Invoke();
+            });
+            app.Use((context, next) =>
+            {
+                PrintCurrentIntegratedPipelineStage(context, "2nd MW");
+                return next.Invoke();
+            });
+
+            /// Configures all the previously registered middleware components (in this case, our two diagnostic components) 
+            /// to run on the authentication stage of the pipeline
+            app.UseStageMarker(PipelineStage.Authenticate);
+        }
+        
+        private void PrintCurrentIntegratedPipelineStage(IOwinContext context, string msg)
+        {
+            var currentIntegratedpipelineStage = HttpContext.Current.CurrentNotification;
+            context.Get<TextWriter>("host.TraceOutput").WriteLine($"Current IIS event: { currentIntegratedpipelineStage } Msg: { msg }");
+        }
+    }
+}
